afc5788c24c1177d27f2ff330ec335f6e14d2272
Akshay Agrawal <akshay2agrawal@gmail.com>
2024-05-26 21:39:26 +0200

    feat: working game

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -2,16 +2,12 @@ var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 var userClickedPattern = [];
 var keyPressed = false;
-
-$(".btn").on("click", function (e) {
-  userChosenColour = this.id;
-  userClickedPattern.push(userChosenColour);
-
-  animatePress(userChosenColour);
-  playSound(userChosenColour);
-});
+var level = 0;
 
 function nextSequence() {
+  userClickedPattern = [];
+  $("h1").text("Level " + level);
+  level++;
   var randomNumber = Math.floor(Math.random() * 4);
   randomChosenColour = buttonColors[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -35,10 +31,48 @@ function animatePress(currentColour) {
   }, 100);
 }
 
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (currentLevel === gamePattern.length - 1) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    const gameOver = new Audio("./sounds/wrong.mp3");
+    gameOver.play();
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over");
+    resetGame();
+  }
+}
+
+function resetGame() {
+  setTimeout(function () {
+    $("h1").text("Press A Key to Start");
+    keyPressed = false;
+    gamePattern = [];
+    level = 0;
+  }, 1000);
+}
+
+$(".btn").on("click", function (e) {
+  if (keyPressed) {
+    var userChosenColour = this.id;
+    animatePress(userChosenColour);
+    playSound(userChosenColour);
+
+    userClickedPattern.push(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+  }
+});
+
 $(document).on("keypress", function () {
   if (!keyPressed) {
     keyPressed = true;
-
     nextSequence();
   }
 });
